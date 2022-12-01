508aa9a52473770b1abddd0448501823ae0cb684
Karimullin Arthur <KarimullinArthur@riseup.net>
2022-12-01 22:15:10 +0500

    Обновляем конфиги

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ~
-ln -s ~/.config/myConfigs/zsh/.antigen.zsh
+ln -s ~/.config/myConfigs/zsh/antigen.zsh
 ln -s ~/.config/myConfigs/zsh/.zshrc
 ln -s ~/.config/myConfigs/tmux/.tmux.conf
 ln -s ~/.config/myConfigs/tmux/.tmux.conf.local
