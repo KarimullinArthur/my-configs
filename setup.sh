41bbd14243de5e1571a437991f5ff7aa4dd0f322
Karimullin Arthur <KarimullinArthur@riseup.net>
2022-12-01 21:56:17 +0500

    Обновляем конфиги add mirror

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ ln -s ~/.config/myConfigs/zsh/.antigen.zsh
 ln -s ~/.config/myConfigs/zsh/.zshrc
 ln -s ~/.config/myConfigs/tmux/.tmux.conf
 ln -s ~/.config/myConfigs/tmux/.tmux.conf.local
+
+cd ~/.config
+
+ln -s ~/.config/myConfigs/sway
